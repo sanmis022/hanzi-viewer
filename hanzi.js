c5ad7505d845150e005305aa12f35275afcf93f3
sanmis022 <santoshm979@gmail.com>
2025-06-24 14:02:07 +1200

    Update hanzi.js

--- hanzi.js
+++ hanzi.js
@@ -3,6 +3,7 @@ const pinyinMap = {
   "好": "hǎo",
   "永": "yǒng"
 };
+
 const characters = Object.keys(pinyinMap);
 
 function createButtons() {
@@ -19,16 +20,12 @@ function createButtons() {
 function drawCharacter(char) {
   const code = char.codePointAt(0).toString(16);
   document.getElementById("pinyin").innerText = pinyinMap[char] || "";
+
   fetch(`hanzi-data/${code}.json`)
     .then(res => res.json())
     .then(data => {
       const strokes = data.strokes;
-      const paths = strokes.map((d, i) => `
-        <path d="${d}" fill="none" stroke="black" stroke-width="3">
-          <animate attributeName="stroke-dashoffset" from="1000" to="0" dur="0.5s" begin="${i * 0.6}s" fill="freeze" />
-          <animate attributeName="stroke-dasharray" from="0,1000" to="1000,0" dur="0.5s" begin="${i * 0.6}s" fill="freeze" />
-        </path>
-      `).join('');
+      const paths = strokes.map(d => `<path d="${d}" fill="none" stroke="black" stroke-width="3" />`).join('');
       const svg = `
         <svg viewBox="0 0 1024 1024" width="300" height="300" xmlns="http://www.w3.org/2000/svg">
           <g transform="scale(1, -1) translate(0, -1024)">
@@ -37,6 +34,16 @@ function drawCharacter(char) {
         </svg>
       `;
       document.getElementById("svg-container").innerHTML = svg;
+
+      const svgElement = document.querySelector("#svg-container svg");
+      const pathElements = svgElement.querySelectorAll("path");
+
+      pathElements.forEach((path, i) => {
+        const length = path.getTotalLength();
+        path.style.strokeDasharray = length;
+        path.style.strokeDashoffset = length;
+        path.style.animation = `draw 0.6s ease-out ${i * 0.6}s forwards`;
+      });
     })
     .catch(() => {
       document.getElementById("svg-container").innerHTML = "<p style='color:red;'>Stroke data not found.</p>";
